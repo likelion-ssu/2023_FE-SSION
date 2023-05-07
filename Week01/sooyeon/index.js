14dd8e295a90d4e722c8078c06b2c38fc5c90859
SooY2 <ssy67366736@gmail.com>
2023-05-08 02:57:05 +0900

    add:1week task

---
+++ Week01/sooyeon/index.js
@@ -0,0 +1,49 @@
+﻿// //변수 선언
+// const firstname = true;
+// let lastname = "신";
+
+// console.log(firstname, lastname);
+
+// //변수 타입 확인
+// console.log(typeof firstname);
+
+//배열 선언하기
+// let arr1 = ["준규","상민","효민"];
+// let arr2 = new Array();
+
+// console.log(arr1);
+
+// arr1[0]="지원";
+// arr1[1]="수연";
+// arr1[2]="해솔";
+
+// arr2[0]="유은";
+// arr2[1]="동윤"
+
+// console.log(arr1);
+// console.log(arr1.length);
+// console.log(arr2.length);
+
+//명시적 타입 변환
+// const tmp=10;
+// const tmp1 = String(tmp);
+
+// console.log(typeof(tmp1));
+
+// const tmp2="10";
+// const tmp3=Number(tmp2);
+// console.log(typeof(tmp3));
+
+// let arr8=[1,2,3];
+// for(var value of arr8){
+//   console.log(value);//1 2 3
+// }
+let arr=[1,2,3];
+
+let filter1 = arr.filter(x=>x<3);
+
+console.log(filter1);
+
+
+
+
